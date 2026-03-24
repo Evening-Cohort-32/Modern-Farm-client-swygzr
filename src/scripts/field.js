cede197c4eec724d64299e647c9dbf793b355839
mramey-dev <rameykenya93@gmail.com>
2026-03-23 20:15:13 -0500

    save progress before syncing

--- src/scripts/field.js
+++ src/scripts/field.js
@@ -1,15 +1,22 @@
 //Create a scripts/field.js module. COMPLETE
-// Define a variable in this module whose initial value is an empty array. This array will store all of the plants that are growing in the field. Do not export the array.
+// Define a variable in this module whose initial value is an empty array. This array will store all of the plants that are growing in the field. ******* Do not export the array *******
 
-//const crop = []
+const growingPlants = [];
 
 //In the module, define and export a function named addPlant.The addPlant function must accept a seed object as input. The function will add the seed to the field (
 
-//export const addPlant = (seed) => {}
+export const addPlant = (seeds) => {
+  for (const seed of seeds) {
+    growingPlants += seed;
+  }
+  return growingPlants;
+};
 
 //Define and export a function named usePlants that returns a copy of the array of plants.
 
-//export const usePlants = structuredClone()
+export const usePlants = () => {
+  return structuredClone(growingPlants);
+};
 
 /*
 Checking Your Logic
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,6 @@
 import { createPlan } from "./plan";
+import { addPlant, usePlants } from "./field";
 
-
-
-console.log("Welcome to the main module")
+console.log("Welcome to the main module");
 const yearlyPlan = createPlan();
 console.log("Plan created:", yearlyPlan);
