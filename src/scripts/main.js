a9891a97b2d8bb623b7bbdf244ec7b5bb648e02d
Woody Maddox <47863137+woodymaddox@users.noreply.github.com>
2026-03-23 19:27:43 -0500

    Revert "Revert "Commit all local changes""

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,10 @@
 import { createPlan } from "./plan";
-
+import { createAsparagus } from "./seeds/asparagus";
 
 
 console.log("Welcome to the main module")
 const yearlyPlan = createPlan();
 console.log("Plan created:", yearlyPlan);
+
+const asparagus = createAsparagus();
+console.log("Asparagus created:", asparagus);
